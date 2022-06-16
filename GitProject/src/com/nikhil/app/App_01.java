4cd0a213ae410437bda399a95207af614893bfdf
Nikhil Gupta <nikhilgahoi1731997@gmail.com>
2022-06-16 23:07:28 +0530

    commit 2: added 2nd file

--- GitProject/src/com/nikhil/app/App_01.java
+++ GitProject/src/com/nikhil/app/App_01.java
@@ -3,6 +3,6 @@ package com.nikhil.app;
 public class App_01 {
 	public static void main() {
 		System.out.println("Hello Nikhil");
-		System.out.println("Welcome to nikhil world!!!");
+		System.out.println("Welcome to Nikhil world!!!");
 	}
 }
---
+++ GitProject/src/com/nikhil/app/App_02.java
@@ -0,0 +1,10 @@
+package com.nikhil.app;
+
+public class App_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to 2nd program");
+	}
+
+}
