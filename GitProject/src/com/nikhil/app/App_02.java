703d329356528d103a4ee8714465f90775ebfbd9
Nikhil Gupta <nikhilgahoi1731997@gmail.com>
2022-06-16 23:11:26 +0530

    commit 3: thank you msg on 2nd file

--- GitProject/src/com/nikhil/app/App_02.java
+++ GitProject/src/com/nikhil/app/App_02.java
@@ -5,6 +5,7 @@ public class App_02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to 2nd program");
+		System.out.println("thank you");
 	}
 
 }
